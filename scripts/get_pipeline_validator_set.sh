030ac1ec08ca505e537cdbc72217cf56f264d6dc
brentstone <brentstone01@gmail.com>
2024-12-22 22:00:07 -0800

    update validator set script to take num_vals as input

--- scripts/get_pipeline_validator_set.sh
+++ scripts/get_pipeline_validator_set.sh
@@ -1,10 +1,12 @@
+# Usage: ./get_pipeline_validator_set.sh $num_vals
+# num_vals: the top $num_vals validators will be displayed
+num_vals=$1
+	
 epoch=$(namadac epoch --node $RPC_NAMADA_MAINNET | cut -d' ' -f4)
 pipeline_epoch=$(($epoch + 2))
 echo "Pipeline epoch right now = $pipeline_epoch"
 namadac bonded-stake --epoch $pipeline_epoch --node $RPC_NAMADA_MAINNET > validator_set_pipeline.out
 total_stake=$(cat validator_set_pipeline.out | grep 'Total bonded' | cut -d' ' -f4)
-
-num_vals=25
 
 echo "\nTop $num_vals validators by fractional stake:"
 for line in $(cat validator_set_pipeline.out| grep tnam | head -n $num_vals | tr -d ' '); do
